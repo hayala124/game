ba947f39059de7d2a04ba7f4e8e1072e990305fe
hayala124 <elnur7803@gmail.com>
2024-05-28 20:21:30 +0300

    Добавлены видео и закончен тест

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ android {
 
 dependencies {
     implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
--- app/src/main/java/com/hayala/game/TestActivity.kt
+++ app/src/main/java/com/hayala/game/TestActivity.kt
@@ -22,14 +22,14 @@ class TestActivity : AppCompatActivity() {
     val questions = listOf(
         Question( "Как называется именнованная область памяти, в которой хранится значение определенного типа?\n(1 - константа, 2 - переменная, 3 - память)", "2"),
         Question("Выберите тип данных, который может хранить информацию почти любого типа данных?\n(1 - int, 2 - char, 3 - object, 4 - bool, 5 - string)", "3"),
-        Question("Как называется приём присвоения переменной значения при определении переменной?\n(1 - именование, 2 - инициализация, 3 - объявление, 4 - нет такого понятия)", "2"),
+       /* Question("Как называется приём присвоения переменной значения при определении переменной?\n(1 - именование, 2 - инициализация, 3 - объявление, 4 - нет такого понятия)", "2"),
         Question("Как называется определение, которое обозначает постоянные значения, которые известны во время компиляции и не изменяются во время выполнения программы?\n(1 - константа, 2 - переменная, 3 - не существует такого понятия", "1"),
         Question("Как обозначается знак для получения остатка от деления?\n(1 - \\, 2 - *, 3 - >>, 4 - %)", "4"),
-        Question("Верно ли утрверждение, что \"Continue пропускает итерацию, break выходит из цикла?\"\n(1 - да, 2 - нет)", "1"),
-        Question("", ""),
-        Question("", ""),
-        Question("", ""),
-        Question("", "")
+        Question("Верно ли утрверждение, что \"Continue пропускает итерацию, break выходит из цикла?\"\n(1 - да, 2 - нет, наоборот)", "1"),
+        Question("Верно ли что это || - операция логического умножения и возвращает true, если оба операнда одновременно равны true?\n(1 - да, 2 - нет, это логическое сложение)", "2"),
+        Question("Конструкция try-catch работает с ..?\n(1 - файлами, 2 - исключениями, 3 - базой данных, 3 - классами) ", "2"),
+        Question("Правильно ли создан данный массив: int[] arr = new int [2] {2, 5};?\n(1 - да, 2 - нет)", "1"),*/
+        Question("Правильно ли создана переменная int num = \"1\";\n(1 - да, 2 - нет)", "2")
         )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,13 +68,16 @@ class TestActivity : AppCompatActivity() {
                 if (countQuestions == questions.size - 1) {
                     buttonNextQuestion.text = "Завершить тест"
                 }
-            } else {
+            } else if (countQuestions == questions.size){
+
                 buttonNextQuestion.text = "Выйти"
-                questionTextView.visibility = View.GONE
                 questionTextView.visibility = View.INVISIBLE
+                binding.textTest.visibility = View.INVISIBLE
                 answerEditText.visibility = View.INVISIBLE
                 binding.textResult.text = "Правильных ответов ${countRightAnswers} из ${questions.size}"
             }
+            else
+                finish()
         }
     }
 }
